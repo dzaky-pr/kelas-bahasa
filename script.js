5d9ee0733fd8d94af86952a349492efe02cf101b
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-04-21 09:17:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //disable cut copy paste
-$(document).ready(function() {
-	$('body').bind('cut copy paste', function (e) {
-		e.preventDefault();
-	})
-	$("body").on("contextmenu", function(e) {
-		return false;
-	})
-})
+// $(document).ready(function() {
+// 	$('body').bind('cut copy paste', function (e) {
+// 		e.preventDefault();
+// 	})
+// 	$("body").on("contextmenu", function(e) {
+// 		return false;
+// 	})
+// })
